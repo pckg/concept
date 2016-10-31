ff79f03ce11e3087d06955e2c4137a92b27eb3d2
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-31 22:33:29 +0100

    Fix

--- src/Pckg/Concept/Context.php
+++ src/Pckg/Concept/Context.php
@@ -93,7 +93,7 @@ class Context
      */
     public function getOrDefault($key, $default = null)
     {
-        if (array_key_exists($key, $this->data)) {
+        if (!array_key_exists($key, $this->data)) {
             return $default;
         }
 
