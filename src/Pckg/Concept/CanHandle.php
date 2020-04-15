86752e5489cbc754d5d9ed11b673ba4242eda25a
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-04-16 01:29:47 +0200

    Allow defaults

--- src/Pckg/Concept/CanHandle.php
+++ src/Pckg/Concept/CanHandle.php
@@ -21,12 +21,12 @@ trait CanHandle
 
     protected function initOverloadMethods()
     {
-        $this->methods = [];
+        $this->methods = $this->methods ?? [];
     }
 
     protected function mergeOverloadMethods($arrMethods)
     {
-        $this->methods = array_unique(array_merge($this->methods ?: [], $arrMethods));
+        $this->methods = array_unique(array_merge($this->methods ?? [], $arrMethods));
     }
 
 }
